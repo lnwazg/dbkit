85317d0d007a5504e235bb319a3712812b89d0ad
李楠 <lnwazg@126.com>
2018-09-13 17:29:30 +0800

    Initial Commit

--- src/main/java/com/lnwazg/dbkit/anno/service/Transactional.java
+++ src/main/java/com/lnwazg/dbkit/anno/service/Transactional.java
@@ -8,7 +8,8 @@ import java.lang.annotation.Target;
 
 /**
  * 加入事务管理控制<br>
- * benchmark 研究发现，开启事务会明显降低程序的执行速度！这也是合乎理论的！因为多了一个开启事务以及commit的过程！<br>
+ * benchmark 研究发现，开启事务会明显降低程序的执行速度！<br>
+ * 但这也是合乎理论的！因为多了一个开启事务以及commit的过程！<br>
  * 框架的事务管理过程已经完美，再也不需要自己手工干预！
  * @author nan.li
  * @version 2016年5月12日
